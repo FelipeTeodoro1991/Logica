06a6226d124bbdf7ee25c35a53d268261784c148
Felipe.teodoro1991 <fteodoro1991@gmail.com>
2020-05-16 21:11:08 -0300

    Atividade 08

--- 11 JS EXERCICIOS LISTA ARRAY/07Atividade.js
+++ 11 JS EXERCICIOS LISTA ARRAY/07Atividade.js
@@ -4,19 +4,17 @@
 // 2 consoantes
 // 3 vogais
 
-var rs = require ("readline-sync");
+var rs = require("readline-sync");
 var palavra = rs.question("Digite uma pavalavra: ").toUpperCase();
 var vogais = "";
-for(i = 0; i < palavra.length ; i++){
-if(palavra[i] == "A" || palavra[i] == "E" || palavra[i] == "I" || palavra[i] == "O" || palavra[i] == "U"){
-    var vogais = vogais + palavra[i]
-}
+for (i = 0; i < palavra.length; i++) {
+    if (palavra[i] == "A" || palavra[i] == "E" || palavra[i] == "I" || palavra[i] == "O" || palavra[i] == "U") {
+        var vogais = vogais + palavra[i]
+    }
 }
 
-var numvogais = vogais.length
-var numcoantes = palavra.length-numvogais
+var numvogais = vogais.length;
+var numcoantes = palavra.length - numvogais;
 
 console.log(`O numeor de vogais é ${numvogais}`);
 console.log(`Onumero de consoantes é ${numcoantes}`);
-
-
